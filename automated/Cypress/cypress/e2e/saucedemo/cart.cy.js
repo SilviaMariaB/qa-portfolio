e9ed2f71e22f56070c6f08a789668fb3e015de73
Bogdan Silvia <bogdansilvia30@gmail.com>
2025-10-15 14:50:21 +0300

    Added initial Cypress setup: e2e folder with login, cart, checkout tests, fixtures for users, and custom commands

--- automated/Cypress/cypress/e2e/saucedemo/cart.cy.js
+++ automated/Cypress/cypress/e2e/saucedemo/cart.cy.js
@@ -1,40 +1,31 @@
 describe('Suita Cart', () => {
     //login with valid account
     beforeEach(()=>{
-        cy.visit('https://www.saucedemo.com/');
-
-        cy.get('[data-test="username"]').type('standard_user');
-        cy.get('[data-test="password"]').type('secret_sauce');
-
-        cy.get('[data-test="login-button"]').click();
+        cy.fixture('users').then((data)=>{
+            cy.login(data.standardUser.username, data.standardUser.password);
+        })
     })
 
     //Check if the right product is added into the cart.
-    it('Add product to cart', ()=> {
-        
+    it('should add a product to the cart', ()=> {
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="shopping-cart-link"]').click();
-
-        cy.get('[data-test="remove-sauce-labs-backpack"]').should('exist');
+        cy.get('[data-test="remove-sauce-labs-backpack"]').should('be.visible');
     });
 
     //Check if the remove button works and products are remove from cart.
-    it('Remove product from cart', ()=> {
-        
+    it('should remove a product from the cart', ()=> {
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="shopping-cart-link"]').click();
         cy.get('[data-test="remove-sauce-labs-backpack"]').click();
-
         cy.get('[data-test="remove-sauce-labs-backpack"]').should('not.exist');
     });
 
     //Check if the Continue Shopping button from cart page works.
-    it('Continue shopping from cart', ()=> {
-        
+    it('should return to shopping when clicking Continue Shopping', ()=> {
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="shopping-cart-link"]').click();
         cy.get('[data-test="continue-shopping"]').click();
-
         cy.get('[data-test="inventory-container"]').should('exist');
     });
 
--- automated/Cypress/cypress/e2e/saucedemo/checkout.cy.js
+++ automated/Cypress/cypress/e2e/saucedemo/checkout.cy.js
@@ -1,42 +1,37 @@
 describe('Suita Checkout', () => {
     //login with valid account
     beforeEach(()=>{
-        cy.visit('https://www.saucedemo.com/');
-
-        cy.get('[data-test="username"]').type('standard_user');
-        cy.get('[data-test="password"]').type('secret_sauce');
-
-        cy.get('[data-test="login-button"]').click();
+        cy.fixture('users').then((data)=>{
+            cy.login(data.standardUser.username, data.standardUser.password)
+        })
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="shopping-cart-link"]').click();
-
         cy.get('[data-test="inventory-item"]').should('exist');
     })
 
     //Check if the checkout works when the user enters valid data.
-    it('Checkout with valid data', ()=> {
+    it('should complete checkout with valid data', ()=> {
         cy.get('[data-test="checkout"]').click();
         cy.get('[data-test="firstName"]').type('Ana');
         cy.get('[data-test="lastName"]').type('Maria');
         cy.get('[data-test="postalCode"]').type('123456');
         cy.get('[data-test="continue"]').click();
         cy.get('[data-test="finish"]').click();
-        cy.get('[data-test="complete-header"]').should('exist');
+        cy.get('[data-test="complete-header"]').should('be.visible');
         
     });
     
     //Check if the Checkout with missing required fields is displaying error message.
-    it('Checkout with missing required fields', ()=> {
+    it('should show error when required fields are missing', ()=> {
         cy.get('[data-test="checkout"]').click();
         cy.get('[data-test="firstName"]').type('Ana');
-        cy.get('[data-test="postalCode"]').type('123456');
         cy.get('[data-test="continue"]').click();
-        cy.get('[data-test="error"]').should('exist');
+        cy.get('[data-test="error"]').should('be.visible');
         
     });
 
     //Check if the cancel button from checkout page works.
-    it('Cancel checkout', ()=> {
+    it('should cancel checkout successfully', ()=> {
         cy.get('[data-test="checkout"]').click();
         cy.get('[data-test="cancel"]').click();
         cy.get('[data-test="inventory-item"]').should('exist');
